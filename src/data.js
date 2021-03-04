b14a336b3135b220b089b98e7933603b878c1a00
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2021-03-04 21:05:21 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,108 @@
+import { v4 as uuidv4 } from "uuid";
+
+function spotify() {
+  return [
+    {
+      name: "Bol Do Na Zara",
+      artist: "Armaan Malik",
+      cover:
+        "https://cdn.pagalworld.icu/1298-bol-do-na-zara.jpg",
+      id: uuidv4(),
+      active: true,
+      color: ["#501718", "#0D2122"],
+      audio: "https://open.spotify.com/track/6eO9LvEiMqh1CAsa6y3wXP?si=2a29518f82b54f6f",
+    },
+    {
+      name: "Buddhu-Sa-Mann",
+      artist: "Armaan Malik",
+      cover:
+        "https://cdn.pagalworld.icu/1276-buddhu-sa-mann-small.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#865446", "#C3AB8D"],
+      audio: "https://open.spotify.com/track/1dxbiVeiV7nQ2V9qXnhJiB?si=0a18d9415b9242e7",
+    },
+    {
+      name: "Titliyan",
+      artist: "Sunidhi Chauhan",
+      cover:
+        "https://cdn.pagalworld.icu/1289-titliyan-small.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C5FDD6", "#D3806E"],
+      audio: "https://open.spotify.com/track/1980E4Grgu5xAhNbFZ1mb1?si=497099c99b014eed",
+    },
+    {
+      name: "	Go Go Govinda",
+      artist: "Shreya Ghoshal",
+      cover:
+        "https://cdn.pagalworld.icu/3609-go-go-govinda-small.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#501718", "#0D2122"],
+      audio: "https://open.spotify.com/track/1UzYybFkJcyvB2hkL129GA?si=87f79851b04740b5",
+    },
+    {
+      name: "Dekha Hazaro Dafaa",
+      artist: "Arijit Singh",
+      cover:
+        "https://cdn.pagalworld.icu/1469-dekha-hazaro-dafaa-small.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#8093FF", "#06004D"],
+      audio: "https://open.spotify.com/track/7AV1hh76pC1mg4iSH796Vp?si=d38c2f3a114c4bf5",
+    },
+    {
+      name: "Babuji Zara Dheere Chalo",
+      artist: "Sonu Kakkar,",
+      cover:
+        "https://cdn.pagalworld.icu/7069-babuji-zara-dheere-chalo-small.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#91C9D8", "#1F1C36"],
+      audio: "https://open.spotify.com/track/6tBw0YriaKSzUr5M8xXH2H?si=9687c21aa97b44f8",
+    },
+    {
+      name: "Aankh Marey",
+      artist: "Kumar Sanu",
+      cover:
+        "https://cdn.pagalworld.icu/689-aankh-marey-small.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#E29C97", "#601B5C"],
+      audio: "https://open.spotify.com/track/63MvWd6T6yoS7h4AJ4Hjrm?si=7f928e47268a4bc7",
+    },
+    {
+      name: "Jai Jai Shivshankar",
+      artist: "Vishal Dadlani",
+      cover:
+        "https://cdn.pagalworld.icu/7858-jai-jai-shivshankar-small.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#6C7676", "#3D5172"],
+      audio: "https://open.spotify.com/track/6lfSsCL894Qw15xbt7cSUy?si=c9a30a86cce7408d",
+    },
+    {
+      name: "Bhangra Ta Sajda",
+      artist: "Neha Kakkar",
+      cover:
+        "https://cdn.pagalworld.icu/428-bhangra-ta-sajda-small.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FACEB5", "#C7582B"],
+      audio: "https://open.spotify.com/track/4ms4RFmu8rQRPekGr2ru4S?si=19f7682cb5e74059",
+    },
+    {
+      name: "Manali Trance",
+      artist: "Neha Kakkar",
+      cover:
+        "https://cdn.pagalworld.icu/2686-manali-trance-small.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#E2C6D4", "#133362"],
+      audio: "https://open.spotify.com/track/4n1fgIJ6WtHxfBimbDav8v?si=bf69512803074589",
+    },
+  ];
+}
+
+export default spotify;
